b44ad1c9f7067a82a3ec75b21774ded94e5b673f
nobody396 <284531162+nobody396@users.noreply.github.com>
2026-06-12 20:12:10 +0800

    fix: update cleanup wiring test

--- backend/cmd/server/wire_gen_test.go
+++ backend/cmd/server/wire_gen_test.go
@@ -78,6 +78,7 @@ func TestProvideCleanup_WithMinimalDependencies_NoPanic(t *testing.T) {
 		nil, // scheduledTestRunner
 		nil, // backupSvc
 		nil, // downloadResourceSvc
+		nil, // supplierService
 	)
 
 	require.NotPanics(t, func() {
